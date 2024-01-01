50b0feebdc1e10344f4f3a50c9b96c8733c3e6f9
patoary <zahidsagar93@gmail.com>
2024-01-01 22:48:40 +0900

    file upload system with avatarUpload_&_singleUploader

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -4,10 +4,12 @@ const express = require("express");
 // internal imports
 const { getUsers } = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
-
+const avatarUpload = require("../middlewares/users/avatarUpload");
 const router = express.Router();
 
-//inbox page
+//user page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
 
+//add user
+router.post("/", avatarUpload);
 module.exports = router;
