38efa910f85d7ef33a64edce9586db6030035d93
patoary <zahidsagar93@gmail.com>
2024-01-13 22:07:26 +0900

    add default value for decorateHtmlResponse

--- middlewares/common/decorateHtmlResponse.js
+++ middlewares/common/decorateHtmlResponse.js
@@ -2,6 +2,10 @@ function decorateHtmlResponse(page_title) {
   return function (req, res, next) {
     res.locals.html = true;
     res.locals.title = `${page_title} - ${process.env.APP_NAME}`;
+    // default empty value set korle pore value na thakle o error hobe na
+    res.locals.loggedInUser = {};
+    res.locals.errors = {};
+    res.locals.data = {};
     next();
   };
 }
