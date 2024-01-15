3a94be7b0d18e91e42e492cb0661f903ffbb2205
patoary <zahidsagar93@gmail.com>
2024-01-15 22:34:11 +0900

    implement login and logout in loginController

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,3 +1,11 @@
+//external imports
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
+
+//internal imports
+const User = require("../models/People");
+
 //get login page
 function getLogin(req, res, next) {
   res.render("index");
@@ -10,8 +18,62 @@ async function login(req, res, next) {
     const user = await User.findOne({
       $or: [{ email: req.body.username }, { mobile: req.body.username }],
     });
-  } catch (error) {}
+    if (user && user._id) {
+      const isValidPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+
+      if (isValidPassword) {
+        // prepare to user object to generate token
+        const userObject = {
+          username: user.name,
+          mobile: user.mobile,
+          email: user.email,
+          role: user,
+        };
+
+        //generate token
+        const token = jwt.sign(userObject, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+        });
+        // set cookie for batter security
+        res.cookie(process.env.COOKIE_NAME, token, {
+          maxAge: process.env.ACCESS_TOKEN_EXPIRY,
+          httpOnly: true,
+          signed: true,
+        });
+
+        //set logged in user local identifier
+        res.locals.loggedInUser = userObject;
+
+        res.render("inbox");
+      } else {
+        throw createError("Login failed! Please try again.");
+      }
+    } else {
+      throw createError("Login failed! Please try again.");
+    }
+  } catch (err) {
+    res.render("index", {
+      data: {
+        username: req.body.username,
+      },
+      errors: {
+        common: {
+          msg: err.message,
+        },
+      },
+    });
+  }
+}
+// do logout
+function logout(req, res) {
+  res.clearCookie(process.env.COOKIE_NAME);
+  res.send("logged out");
 }
 module.exports = {
   getLogin,
+  login,
+  logout,
 };
